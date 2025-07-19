aafaa3b68b7bed0b8ef9636e4c67d2856b7f3175
Ikit24 <szszattila2@gmail.com>
2025-07-19 14:13:13 +0300

    refactor:  get_html, crawl_page under AsyncCrawler class

--- crawl.py
+++ crawl.py
@@ -13,7 +13,7 @@ def normalize_url(url):
         return parsed_url.scheme + "://" + netloc + path
 
 
-def get_urls_from_html(html, base_url):
+def get_urls_from_html(html, base_url, base_domain):
     if not isinstance(html, str):
         raise TypeError("HTML is not a string")
 
@@ -23,58 +23,12 @@ def get_urls_from_html(html, base_url):
         href = tag.get("href")
         if href != None:
             absolute_url = urljoin(base_url, href)
-            urls.append(absolute_url)
+            parsed_url = urlparse(absolute_url)
+            netloc = parsed_url.netloc
+            if netloc == base_domain:
+                urls.append(absolute_url)
     return urls
 
-
-def get_html(url):
-    try:
-        r = requests.get(url)
-
-        if r.status_code >= 400:
-            raise Exception(f"HTTP error {r.status_code}: {r.reason}")
-
-        content_type = r.headers.get('content-type', '')
-        if not content_type.startswith('text/html'):
-            raise Exception(f"Content-type is not text/html. Got: {content_type}")
- 
-        return r.text
-
-    except requests.exceptions.RequestException as e:
-        raise Exception(f"Request failed: {str(e)}")
-
-def crawl_page(base_url, current_url=None, pages=None):
-    if pages is None:
-        pages = {}
-
-    if current_url is None:
-        current_url = base_url
-    parsed1 = urlparse(current_url)
-    parsed2 = urlparse(base_url)
-    if not parsed1.netloc == parsed2.netloc:
-        return
-
-    current_url = normalize_url(current_url)
-
-    if current_url in pages:
-        pages[current_url] += 1
-        return
-    else:
-        pages[current_url] = 1
-
-    print(f"Crawling: {current_url}")
-    try:
-        HTML = get_html(current_url)
-    except Exception:
-        return
-    if HTML is None:
-        return
-    URL = get_urls_from_html(HTML, base_url)
-
-    for url in URL:
-        crawl_page(base_url, url, pages)
-
-    return pages
 
 class AsyncCrawler:
     def __init__(self, base_url, base_domain, max_concurrency):
@@ -85,12 +39,59 @@ class AsyncCrawler:
         self.max_concurrency = max_concurrency
         self.semaphore = asyncio.Semaphore(max_concurrency)
 
+
     async def __aenter__(self):
         self.session = aiohttp.ClientSession()
         return self
 
+
     async def __aexit__(self, exc_type, exc_val, exc_tb):
         await self.session.close()
 
+
     async def add_page_visit(self, normalized_url):
-        pass
+        async with self.lock:
+            if normalized_url in self.pages:
+                self.pages[normalized_url] += 1
+                return False
+            else:
+                self.pages[normalized_url] = 1
+                return True
+
+
+    async def get_html(self, url):
+        try:
+            async with self.session.get(url) as response:
+                if response.status_code >= 400:
+                    raise Exception(f"HTTP error {response.status}: {response.reason}")
+
+                content_type = response.headers.get('content-type', '')
+                if not content_type.startswith('text/html'):
+                    raise Exception(f"Content-type is not text/html. Got: {content_type}")
+ 
+                return await response.text()
+
+        except aiohttp.ClientError as e:
+            raise Exception(f"Request failed: {str(e)}")
+
+
+    async def crawl_page(self, url):
+        normalized_url = normalize_url(url)
+        is_new_page = await self.add_page_visit(normalized_url)
+        if not is_new_page:
+            return
+
+        async with self.semaphore:
+            try:
+                HTML = await self.get_html(normalized_url)
+            except Exception:
+                return
+            if HTML is None:
+                return
+            new_urls = get_urls_from_html(HTML, normalized_url, self.base_domain)
+
+            tasks = []
+            for url in new_urls:
+                background_task = asyncio.create_task(self.crawl_page(url))
+                tasks.append(background_task)
+            await asyncio.gather(*tasks)
